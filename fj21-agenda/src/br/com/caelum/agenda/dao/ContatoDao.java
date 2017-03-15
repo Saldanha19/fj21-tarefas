9f48a9899fea88dc4bcdab1515bdeecbfe1ce7ee
icaro <icaro.fsaldanha@gmail.com>
2017-03-14 22:52:14 -0300

    Exercicio de filter 10.2 e 10.6

--- fj21-agenda/src/br/com/caelum/agenda/dao/ContatoDao.java
+++ fj21-agenda/src/br/com/caelum/agenda/dao/ContatoDao.java
@@ -16,6 +16,12 @@ import br.com.caelum.agenda.modelo.Contato;
 public class ContatoDao {
 	private Connection connection;
 
+	public ContatoDao(Connection connection) {
+		
+		this.connection = connection;
+		
+	}
+	
 	public ContatoDao() {
 		try {
 			this.connection = new ConnectionFactory().getConnection();
