21fd897fda20be18c73e06bf7d0474fbafff4bf9
icaro <icaro.fsaldanha@gmail.com>
2017-03-16 22:39:52 -0300

    exercicio 11.12
    Validando tarefas com spring

--- fj21-tarefas/src/br/com/caelum/tarefas/modelo/Tarefa.java
+++ fj21-tarefas/src/br/com/caelum/tarefas/modelo/Tarefa.java
@@ -2,9 +2,17 @@ package br.com.caelum.tarefas.modelo;
 
 import java.util.Calendar;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class Tarefa {
 	private Long id;
+	
+	@NotEmpty @Size(min=5)
 	private String descricao;
+	
 	private boolean finalizado;
 	private Calendar dataFinalizacao;
 
